03a14a550d2422e52e2106a456ae0b2df75f6ffa
Terenzzzz <terence592592@gmail.com>
2024-03-30 23:20:21 +0000

    update

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -17,7 +17,7 @@ var storage = multer.diskStorage({
         var original = file.originalname;
         var file_extension = original.split(".");
         // Make the file name the date + the file extension
-        filename =  Date.now() + '.' + file_extension[file_extension.length-1];
+        filename =  req.body.email + '.' + file_extension[file_extension.length-1];
         cb(null, filename);
     }
 });
